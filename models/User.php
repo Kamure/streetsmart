a67da67457b85f525951a4af23bb2ac74357eaaa
kamure beyonce kabura <beyonce.kamure@strathmore.edu>
2025-10-28 08:38:57 +0300

    Created user model

---
+++ models/User.php
@@ -0,0 +1,19 @@
+<?php
+class User {
+    private $pdo;
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function create($name, $email, $phone, $password, $role) {
+        $stmt = $this->pdo->prepare("INSERT INTO users (name, email, phone, password, role) VALUES (?, ?, ?, ?, ?)");
+        return $stmt->execute([
+            $name,
+            $email,
+            $phone,
+            password_hash($password, PASSWORD_DEFAULT),
+            $role
+        ]);
+    }
+}
+?>
