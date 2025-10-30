fd1965e2b769f58e3ff4dbb46116ed4e62ffa6c3
kamure beyonce kabura <beyonce.kamure@strathmore.edu>
2025-10-30 09:01:58 +0300

    Update User.php

--- models/User.php
+++ models/User.php
@@ -15,5 +15,12 @@ class User {
             $role
         ]);
     }
+     public function findByEmail($email) {
+        $stmt = $this->pdo->prepare("SELECT * FROM users WHERE email = ?");
+        $stmt->execute([$email]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+
 }
 ?>
