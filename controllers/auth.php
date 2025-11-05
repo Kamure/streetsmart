4d941aea40a70e4757ab3e22b36c390f043bd059
kamure beyonce kabura <beyonce.kamure@strathmore.edu>
2025-11-05 13:30:19 +0300

    Added OTP generation and  verification and email sending

--- controllers/auth.php
+++ controllers/auth.php
@@ -1,8 +1,17 @@
 <?php
 require_once '../config/db.php';
 require_once '../models/User.php';
+require_once '../models/OtpCode.php';
+
+function generateOTP($length = 6) {
+    return str_pad(rand(0, pow(10, $length)-1), $length, '0', STR_PAD_LEFT);
+}
+
+
 
 $userModel = new User($pdo);
+$otpModel = new OtpCode($pdo);
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_POST['name'];
@@ -14,6 +23,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $success = $userModel->create($name, $email, $phone, $password, $role);
 
     if ($success) {
+         $user = $userModel->findByEmail($email);
+        $user_id = $user['id'];
+
+
+        $otp_code = $otpModel->generate($user_id);
+
+         mail($email, "Your verification code is: $otp");
+
+
+
         echo "User registered successfully.";
     } else {
         echo "Registration failed.";
