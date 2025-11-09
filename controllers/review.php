ffecaeef49edfb42ff6b9f847a2e3104e67da7d1
kamure beyonce kabura <beyonce.kamure@strathmore.edu>
2025-11-09 05:25:35 +0300

    Created review controller for product feedback

---
+++ controllers/review.php
@@ -0,0 +1,16 @@
+<?php
+require_once '../config/db.php';
+require_once '../models/Review.php';
+
+$reviewModel = new Review($pdo);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $product_id = $_POST['product_id'];
+    $customer_id = $_POST['customer_id'];
+    $rating = $_POST['rating'];
+    $comment = $_POST['comment'];
+
+    $reviewModel->create($product_id, $customer_id, $rating, $comment);
+    echo "Review submitted.";
+}
+?> 
